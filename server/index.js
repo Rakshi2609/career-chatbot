bbf7e929d70cc69bfc5394764695866043d79de1
Rakshi2609 <rakshithganjimut@gmail.com>
2025-08-15 19:52:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "career-chatbot-ruby.vercel.app",
     credentials: true
 }));
 app.use(express.json());
